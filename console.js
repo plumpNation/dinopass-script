fc5e8d4a4b67216a68610f40c3d0dffea8b6c947
Gavin King <gavin.king@fronter.com>
2014-10-04 12:14:23 +0200

    initial commit

---
+++ console.js
@@ -0,0 +1,25 @@
+// dinopass has jquery in the page
+var input = $('#password-input');
+var newBtn = $('#simple-button');
+
+var passwords = [];
+
+var interval;
+
+var getNewPassword = function () {
+    password = input.val();
+
+    if (passwords.indexOf(password) < 0) {
+        passwords.push(password);
+    }
+
+    if (passwords.length === 60) {
+        window.clearInterval(interval);
+    }
+
+    newBtn.click();
+};
+
+interval = window.setInterval(getNewPassword, 500);
+
+// Copy and paste the array out of the console and format it how you like in a text editor.
